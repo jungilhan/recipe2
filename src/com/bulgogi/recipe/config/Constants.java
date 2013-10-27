40c6a05e16508e7d56342defd192c9acade49487
Jungil Han <jungil.han@gmail.com>
2013-10-27 16:44:48 +0900

    WP 쿼리 개수 60에서 100으로 수정

--- src/com/bulgogi/recipe/config/Constants.java
+++ src/com/bulgogi/recipe/config/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	public static final String PREF_FACEBOOK_ID = "facebook_id";
 	public static final String PREF_SORT_TYPE = "sort_type";
 
-	public static final int WP_RECIPE_QUERY_COUNT = 60;
+	public static final int WP_RECIPE_QUERY_COUNT = 100;
 	public static final int NAVER_BLOG_QUERY_COUNT = 30;
 
 	public static final int GRIDVIEW_DEFAULT_COLUMNS = 2;
